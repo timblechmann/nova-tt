4566fb02a7b15b521ff432a5308638be4edab1bc
Christof Ressi <christof.ressi@gmx.at>
2024-07-25 16:22:43 +0200

    fix implementation of pause() function
    - use YieldProcessor() with MSVC (works for Intel and ARM64!)
    - use _mm_pause() on all Intel platforms with SSE2
    - use "rep; nop" on Intel platforms without SSE2
    - use "isb" instruction on ARM64
    - use "yield" mnemonic on ARM
    - fall back to busy-waiting otherwise

--- nova-tt/pause.hpp
+++ nova-tt/pause.hpp
@@ -21,33 +21,51 @@
 #ifndef NOVA_TT_PAUSE_HPP
 #define NOVA_TT_PAUSE_HPP
 
-#ifdef __SSE2__
-#include <emmintrin.h>
+#if defined(_MSC_VER)
+#include <Windows.h>
+#elif defined(__x86_64__) || defined(__SSE2__)
+/* amd64 resp. SSE2 */
+#define NOVA_TT_USE_MM_PAUSE
+#include <immintrin.h>
+#elif defined(__i386__) && defined(__GNUC__)
+#define NOVA_TT_USE_REP_NOP
+#elif defined(__aarch64__) && defined(__GNUC__)
+/* ARM64 */
+#define NOVA_TT_USE_ARM_ISB
+#elif defined(__arm__) && defined(__GNUC__) && \
+    (defined(__ARM_ARCH_6K__) || \
+    defined(__ARM_ARCH_6Z__) || \
+    defined(__ARM_ARCH_6ZK__) || \
+    defined(__ARM_ARCH_6T2__) || \
+    defined(__ARM_ARCH_7__) || \
+    defined(__ARM_ARCH_7A__) || \
+    defined(__ARM_ARCH_7R__) || \
+    defined(__ARM_ARCH_7M__) || \
+    defined(__ARM_ARCH_7S__) || \
+    defined(__ARM_ARCH_8A__))
+/* mnemonic 'yield' is supported from ARMv6k onwards */
+#define NOVA_TT_USE_ARM_YIELD
+#else /* fall back to busy-waiting */
+#warning "Cannot pause CPU, falling back to busy-waiting."
 #endif
 
 namespace nova   {
 namespace detail {
 
-#ifdef __SSE2__
-
 static inline void pause()
 {
+#if defined(_MSC_VER)
+	YieldProcessor(); /* expands to pause/yield instruction */
+#elif defined(NOVA_TT_USE_MM_PAUSE)
 	_mm_pause();
-}
-
-#elif defined(__GNUC__) && ( defined(__i386__) || defined(__x86_64__) )
-
-static inline void pause()
-{
+#elif defined(NOVA_TT_USE_REP_NOP)
 	__asm__ __volatile__( "rep; nop" : : : "memory" );
-}
-
-#else
-
-static inline void pause()
-{}
-
+#elif defined(NOVA_TT_USE_ARM_ISB)
+	__asm__ __volatile__("isb");
+#elif defined(NOVA_TT_USE_ARM_YIELD)
+	__asm__ __volatile__("yield");
 #endif
+}
 
 }}
 
